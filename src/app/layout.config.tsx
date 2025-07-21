a748231dfa2c11ff0d9197c5d9311b098216f568
wenhao-macmini <fuwenhao594@163.com>
2025-07-21 17:43:56 +0800

    fix： 文档

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -1,5 +1,6 @@
 import { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 import { i18n } from '@/lib/i18n';
+import { HomeIcon } from 'lucide-react';
 
 // 使 baseOptions 成为一个函数以支持国际化
 export function baseOptions(locale: string): BaseLayoutProps {
@@ -10,14 +11,10 @@ export function baseOptions(locale: string): BaseLayoutProps {
     },
     links: [
       {
-        text: locale === 'zh' ? '文档' : 'Documentation',
-        url: `/${locale}/docs`,
-        active: 'nested-url',
-      },
-      {
-        text: locale === 'zh' ? '博客' : 'Blog',
-        url: `/${locale}/blog`,
-        active: 'nested-url',
+        icon: <HomeIcon />,
+        text: locale === 'zh' ? '主页' : 'Home',
+        url: `/${locale}`,
+        external: true,
       },
     ],
   };
